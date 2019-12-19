9c7eabd4771141fdbd32dd1d93a58ed31800a566
jedforrester-orange <jed_forrester@homedepot.com>
2019-12-19 14:58:33 -0800

    adding header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import HomePage from './pages/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
+
 
 const OopsPage = () => (
   <div>Oops page doesn't exist</div>
@@ -12,6 +14,7 @@ const OopsPage = () => (
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
